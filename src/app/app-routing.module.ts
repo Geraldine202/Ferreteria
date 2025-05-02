a87bf8bcb3537d00778516d9e5814f512935698e
DaphneCuadra <dap.cuadra@duocuc.cl>
2025-05-02 18:26:52 -0400

    Primeros avances Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {
     path: '',
@@ -15,6 +16,7 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+
 
 
 ];
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -10,4 +10,29 @@ export class HomePage {
 
   constructor() {}
 
+
+
+  // Método para iniciar el contador
+ 
+
+  imagenes: string[] = [
+    'assets/images/relleno1.png',
+    'assets/images/relleno1.png',
+    'assets/images/relleno1.png',
+  ];
+ 
+  currentIndex: number = 0; // Índice de la imagen actual
+
+  ngAfterViewInit() {
+    // Establecer un intervalo para cambiar la imagen cada 3 segundos
+    setInterval(() => {
+      this.nextImage();
+    }, 3000); // Cambiar imagen cada 3 segundos
+  }
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex + 1) % this.imagenes.length; // Incrementar índice y volver al principio al llegar al final
+  }
+
+
 }
