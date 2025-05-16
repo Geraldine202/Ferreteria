b9589fcb9878fb44355a246ed7c262edad0a536b
DaphneCuadra <dap.cuadra@duocuc.cl>
2025-05-16 19:39:39 -0400

    Base formulario registro

--- src/app/pages/registrarse/registrarse.page.ts
+++ src/app/pages/registrarse/registrarse.page.ts
@@ -1,17 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-registrarse',
   templateUrl: './registrarse.page.html',
   styleUrls: ['./registrarse.page.scss'],
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, CommonModule,FormsModule],
   standalone: true
 })
 export class RegistrarsePage implements OnInit {
+  usuario = {
+    rut_usuario: '',
+    nombre: '',
+    primer_apellido: '',
+    segundo_apellido: '',
+    genero: '',
+    correo: '',
+    direccion: '',
+    telefono: null,
+    fecha_nacimiento: '',
+    id_comuna: null
+  };
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  onSubmit() {
+  
+    console.log('Formulario enviado', this.usuario);
+
+   
+    this.router.navigate(['/login']);
+  }
 
   ngOnInit() {
   }
