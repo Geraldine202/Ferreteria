ee1b769fd9895e79c7e2c8960fcea6786910cdaf
DaphneCuadra <dap.cuadra@duocuc.cl>
2025-05-16 20:08:58 -0400

    Recuperar Contraseña listo sin Logica

--- src/app/pages/recuperar/recuperar.page.ts
+++ src/app/pages/recuperar/recuperar.page.ts
@@ -1,21 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recuperar',
   templateUrl: './recuperar.page.html',
   styleUrls: ['./recuperar.page.scss'],
+  standalone: true,
   imports: [CommonModule, FormsModule, IonicModule, RouterModule],
-  standalone: true
 })
-export class RecuperarPage implements OnInit {
+export class RecuperarPage {
+  @ViewChild('recoverForm') recoverForm!: NgForm;
+  email: string = '';
 
-  constructor() { }
+  constructor(private toastController: ToastController) {}
 
-  ngOnInit() {
+  async onRecover() {
+    if (this.recoverForm?.valid) {
+      // Simula envío
+      console.log('Enviando instrucciones a:', this.email);
+
+      // Limpia el formulario
+      this.recoverForm.resetForm();
+      this.email = '';
+
+      // Muestra Toast
+      const toast = await this.toastController.create({
+        message: 'Instrucciones enviadas al correo electrónico.',
+        duration: 3000,
+        color: 'success',
+        position: 'top',
+        animated: true,
+      });
+
+      await toast.present();
+    }
   }
-
 }
