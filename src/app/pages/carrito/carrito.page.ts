bea2aea30eab5a80111d659654e4b0575b67de9d
DaphneCuadra <dap.cuadra@duocuc.cl>
2025-05-16 20:18:07 -0400

    Redireccion del boton a carrito y relleno temporal sin funcion

--- src/app/pages/carrito/carrito.page.ts
+++ src/app/pages/carrito/carrito.page.ts
@@ -11,9 +11,23 @@ import { IonicModule } from '@ionic/angular';
 })
 export class CarritoPage implements OnInit {
 
+  carrito = [
+    { nombre: 'Martillo', descripcion: 'Martillo de acero', precio: 5990, cantidad: 1 },
+    { nombre: 'Destornillador', descripcion: 'Philips mediano', precio: 2990, cantidad: 2 },
+    { nombre: 'Serrucho', descripcion: 'Para madera', precio: 8490, cantidad: 1 }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  calcularTotal(): number {
+    return this.carrito.reduce((total, item) => total + item.precio * item.cantidad, 0);
+  }
+
+  finalizarCompra() {
+    console.log('Compra finalizada:', this.carrito);
+    // Aquí podrías limpiar el carrito o redirigir
+  }
 }
