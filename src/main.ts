a3c2c3ca560b7f8043407e2858feae233dfdbf72
DaphneCuadra <dap.cuadra@duocuc.cl>
2025-05-27 19:32:52 -0400

    Informe Excel con LOGO

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,6 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+(window as any).process = require('process/browser');
+(window as any).Buffer = require('buffer').Buffer;
